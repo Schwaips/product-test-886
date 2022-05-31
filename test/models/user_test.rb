71ab3df0fbe08172744adedea0da99a47703dbce
Charlie Bertrand <charlie.bertrand@live.com>
2022-05-31 15:55:37 +0200

    setup github work plus show new create for products

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name returns the capitalized first name and last name" do
+    # setup
+    user = User.new(first_name: "john", last_name: "lennon")
+
+    # exercice - verify
+    assert_equal "John Lennon", user.full_name
+  end
 end
